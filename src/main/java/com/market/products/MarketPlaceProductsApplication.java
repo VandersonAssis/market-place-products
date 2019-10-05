78df08525e5bea6b90196c5218e037c2e2f1bc1d
VandersonAssis <assis.vanderson@gmail.com>
2019-10-04 21:43:16 -0300

    - Initial commit

---
+++ src/main/java/com/market/products/MarketPlaceProductsApplication.java
@@ -0,0 +1,13 @@
+package com.market.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPlaceProductsApplication.class, args);
+	}
+
+}
